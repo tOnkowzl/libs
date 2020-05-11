4e95e26f6f5e7c19b05f0e5f55a9bc4cd8d24740
tOnkowzl <wpansophon@gmail.com>
2020-05-11 09:29:56 +0700

    update chartset to content-type

--- client/const.go
+++ client/const.go
@@ -3,9 +3,9 @@ package client
 // Header const
 const (
 	ContentType     = "Content-Type"
-	ApplicationJSON = "application/json"
-	ApplicationXML  = "application/xml"
-	TextXML         = "text/xml"
+	ApplicationJSON = "application/json; charset=utf-8"
+	ApplicationXML  = "application/xml; charset=utf-8"
+	TextXML         = "text/xml; charset=utf-8"
 
 	HeaderXRequestID    = "X-Request-ID"
 	HeaderAuthorization = "Authorization"

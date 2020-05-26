4388ffe589a42ec09978abac18676119a2530242
tOnkowzl <wpansophon@gmail.com>
2020-05-26 13:54:24 +0700

    add gorm logger

---
+++ logx/gorm.go
@@ -0,0 +1,27 @@
+package logx
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+type GormLogger struct{}
+
+func NewGormLogger() *GormLogger {
+	return new(GormLogger)
+}
+
+func (*GormLogger) Print(v ...interface{}) {
+	switch v[0] {
+	case "sql":
+		Logger.WithFields(logrus.Fields{
+			"module":        "gorm",
+			"type":          "sql",
+			"rows_returned": v[5],
+			"src":           v[1],
+			"values":        v[4],
+			"duration":      v[2],
+		}).Info(v[3])
+	case "log":
+		Logger.WithFields(logrus.Fields{"module": "gorm", "type": "log"}).Print(v[2])
+	}
+}

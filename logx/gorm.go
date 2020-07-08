8550fc5a38233680835c4b1966d1ff5333aeb22f
tOnkowzl <wpansophon@gmail.com>
2020-07-08 13:43:43 +0700

    update to gorm v2

--- logx/gorm.go
+++ logx/gorm.go
@@ -1,28 +1,133 @@
 package logx
 
 import (
+	"context"
+	"log"
+	"os"
 	"time"
 
-	"github.com/sirupsen/logrus"
+	glogger "gorm.io/gorm/logger"
+	"gorm.io/gorm/utils"
 )
 
-type GormLogger struct{}
+type contextKey int
 
-func NewGormLogger() *GormLogger {
-	return new(GormLogger)
+const (
+	ID contextKey = iota
+)
+
+const (
+	Reset       = "\033[0m"
+	Red         = "\033[31m"
+	Green       = "\033[32m"
+	Yellow      = "\033[33m"
+	Blue        = "\033[34m"
+	Magenta     = "\033[35m"
+	Cyan        = "\033[36m"
+	White       = "\033[37m"
+	MagentaBold = "\033[35;1m"
+	RedBold     = "\033[31;1m"
+	YellowBold  = "\033[33;1m"
+)
+
+var DefaultGormLogger = New(log.New(os.Stdout, "\r\n", log.LstdFlags), glogger.Config{
+	SlowThreshold: 100 * time.Millisecond,
+	LogLevel:      glogger.Info,
+	Colorful:      false,
+})
+
+func New(writer glogger.Writer, config glogger.Config) glogger.Interface {
+	var (
+		infoStr      = "%s\n[info] "
+		warnStr      = "%s\n[warn] "
+		errStr       = "%s\n[error] "
+		traceStr     = "%s\n[%.3fms] [rows:%d] %s"
+		traceWarnStr = "%s\n[%.3fms] [rows:%d] %s"
+		traceErrStr  = "%s %s\n[%v] [rows:%d] %s"
+	)
+
+	if config.Colorful {
+		infoStr = Green + "%s\n" + Reset + Green + "[info] " + Reset
+		warnStr = Blue + "%s\n" + Reset + Magenta + "[warn] " + Reset
+		errStr = Magenta + "%s\n" + Reset + Red + "[error] " + Reset
+		traceStr = Green + "%s\n" + Reset + Yellow + "[%.3fms] " + Blue + "[rows:%d]" + Reset + " %s"
+		traceWarnStr = Green + "%s\n" + Reset + RedBold + "[%.3fms] " + Yellow + "[rows:%d]" + Magenta + " %s" + Reset
+		traceErrStr = RedBold + "%s " + MagentaBold + "%s\n" + Reset + Yellow + "[%.3fms] " + Blue + "[rows:%d]" + Reset + " %s"
+	}
+
+	return &logger{
+		Writer:       writer,
+		Config:       config,
+		infoStr:      infoStr,
+		warnStr:      warnStr,
+		errStr:       errStr,
+		traceStr:     traceStr,
+		traceWarnStr: traceWarnStr,
+		traceErrStr:  traceErrStr,
+	}
 }
 
-func (*GormLogger) Print(v ...interface{}) {
-	switch v[0] {
-	case "sql":
-		logrus.WithFields(logrus.Fields{
-			"rows_returned": v[5],
-			"src":           v[1],
-			"values":        v[4],
-			"duration":      v[2].(time.Duration).String(),
-			"stmt":          v[3],
-		}).Info("gorm response information")
-	case "log":
-		logrus.WithFields(logrus.Fields{"data": v[2]}).Print("gorm log")
+type logger struct {
+	glogger.Writer
+	glogger.Config
+	infoStr, warnStr, errStr            string
+	traceStr, traceErrStr, traceWarnStr string
+}
+
+// LogMode log mode
+func (l *logger) LogMode(level glogger.LogLevel) glogger.Interface {
+	newlogger := *l
+	newlogger.LogLevel = level
+	return &newlogger
+}
+
+// Info print info
+func (l logger) Info(ctx context.Context, msg string, data ...interface{}) {
+	if l.LogLevel >= glogger.Info {
+		WithID(ctxID(ctx)).Printf(msg, data...)
 	}
+}
+
+// Warn print warn messages
+func (l logger) Warn(ctx context.Context, msg string, data ...interface{}) {
+	if l.LogLevel >= glogger.Warn {
+		WithID(ctxID(ctx)).Printf(msg, data...)
+	}
+}
+
+// Error print error messages
+func (l logger) Error(ctx context.Context, msg string, data ...interface{}) {
+	if l.LogLevel >= glogger.Error {
+		WithID(ctxID(ctx)).Printf(msg, data...)
+	}
+}
+
+// Trace print sql message
+func (l logger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	if l.LogLevel > 0 {
+		elapsed := time.Now().Sub(begin)
+		switch {
+		case err != nil && l.LogLevel >= glogger.Error:
+			sql, rows := fc()
+			WithID(ctxID(ctx)).Printf(l.traceErrStr, utils.FileWithLineNum(), err, float64(elapsed.Nanoseconds())/1e6, rows, sql)
+		case elapsed > l.SlowThreshold && l.SlowThreshold != 0 && l.LogLevel >= glogger.Warn:
+			sql, rows := fc()
+			WithID(ctxID(ctx)).Printf(l.traceWarnStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, rows, sql)
+		case l.LogLevel >= glogger.Info:
+			sql, rows := fc()
+			WithID(ctxID(ctx)).Printf(l.traceStr, utils.FileWithLineNum(), float64(elapsed.Nanoseconds())/1e6, rows, sql)
+		}
+	}
+}
+
+func WithCtxID(id string) context.Context {
+	return context.WithValue(context.Background(), ID, id)
+}
+
+func ctxID(ctx context.Context) string {
+	if id, ok := ctx.Value(ID).(string); ok {
+		return id
+	}
+
+	return ""
 }

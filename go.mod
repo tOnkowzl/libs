98008278576f31cf3a98cb3f8404fa7f8ba236d6
tOnkowzl <wpansophon@gmail.com>
2020-07-31 18:59:27 +0700

    add pub package

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/tOnkowzl/libs
 go 1.14
 
 require (
+	cloud.google.com/go/pubsub v1.6.1
 	github.com/go-redis/redis/v8 v8.0.0-beta.7
 	github.com/google/uuid v1.1.1
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.6.1
+	google.golang.org/api v0.29.0
 	gorm.io/gorm v0.2.26
 )
---
+++ pub/pub.go
@@ -0,0 +1,69 @@
+package pub
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/tOnkowzl/libs/logx"
+	"google.golang.org/api/option"
+)
+
+type Config struct {
+	Timeout     time.Duration
+	ProjectID   string
+	TopicID     string
+	Credentials string
+}
+
+type Pub struct {
+	Topic     *pubsub.Topic
+	Timeout   time.Duration
+	TopicID   string
+	ProjectID string
+}
+
+func New(cfg Config) *Pub {
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.Timeout)
+	defer cancel()
+
+	client, err := pubsub.NewClient(ctx,
+		cfg.ProjectID,
+		option.WithCredentialsJSON([]byte(cfg.Credentials)))
+	if err != nil {
+		logx.Panic(err)
+	}
+
+	return &Pub{
+		Topic:     client.Topic(cfg.TopicID),
+		Timeout:   cfg.Timeout,
+		TopicID:   cfg.TopicID,
+		ProjectID: cfg.ProjectID,
+	}
+}
+
+func (p *Pub) Publish(ctx context.Context, i interface{}) error {
+	b, err := json.Marshal(i)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	logx.WithContext(ctx).WithFields(logrus.Fields{
+		"topicID":   p.TopicID,
+		"projectID": p.ProjectID,
+		"value":     string(b),
+	}).Info("publish information")
+
+	pubCtx, cancel := context.WithTimeout(context.Background(), p.Timeout)
+	defer cancel()
+
+	res := p.Topic.Publish(pubCtx, &pubsub.Message{Data: b})
+	if _, err = res.Get(pubCtx); err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}

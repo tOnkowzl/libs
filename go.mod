bc17e87139a2add1f663e6c1d2b5e9010d3f66a3
tOnkowzl <wpansophon@gmail.com>
2020-07-31 18:47:11 +0700

    add redis

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/tOnkowzl/libs
 go 1.14
 
 require (
+	github.com/go-redis/redis/v8 v8.0.0-beta.7
 	github.com/google/uuid v1.1.1
 	github.com/labstack/echo/v4 v4.1.16
+	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.6.1
 	gorm.io/gorm v0.2.26
---
+++ redis/redis.go
@@ -0,0 +1,67 @@
+package redis
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/tOnkowzl/libs/logx"
+)
+
+type Options struct {
+	redis.Options
+}
+
+type Client struct {
+	client *redis.Client
+}
+
+func NewClient(opt *Options) *Client {
+	return &Client{
+		client: redis.NewClient(&opt.Options),
+	}
+}
+
+func (c *Client) Get(ctx context.Context, key string) *Bind {
+	res, err := c.client.Get(ctx, key).Result()
+
+	logx.WithContext(ctx).WithFields(logrus.Fields{
+		"key":   key,
+		"value": res,
+	}).Info("redis get information")
+
+	return &Bind{
+		Result: res,
+		Err:    errors.WithStack(err),
+	}
+}
+
+func (c *Client) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	logx.WithContext(ctx).WithFields(logrus.Fields{
+		"key":   key,
+		"value": value,
+	}).Info("redis set information")
+
+	if err := c.client.Set(ctx, key, value, expiration).Err(); err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
+
+type Bind struct {
+	Result string
+	Err    error
+}
+
+func (b *Bind) Bind(i interface{}) error {
+	if b.Err != nil {
+		return b.Err
+	}
+
+	err := json.Unmarshal([]byte(b.Result), i)
+	return errors.WithStack(err)
+}

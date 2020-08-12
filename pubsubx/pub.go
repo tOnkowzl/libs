4d8ddc0a8ec58b5d2cca5a3e5d0cc6b66e6c969a
tOnkowzl <wpansophon@gmail.com>
2020-08-12 15:12:28 +0700

    refactor pubsubx

---
+++ pubsubx/pub.go
@@ -0,0 +1,62 @@
+package pubsubx
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/tOnkowzl/libs/logx"
+)
+
+type PubConfig struct {
+	Client    *pubsub.Client
+	Timeout   time.Duration
+	ProjectID string
+	TopicID   string
+}
+
+type Pub struct {
+	Topic     *pubsub.Topic
+	Timeout   time.Duration
+	TopicID   string
+	ProjectID string
+}
+
+func NewPub(cfg PubConfig) *Pub {
+	if cfg.Client == nil || cfg.Timeout == 0 || cfg.TopicID == "" || cfg.ProjectID == "" {
+		logx.Panic("pubsub client, timeout, topicID, projectID required")
+	}
+
+	return &Pub{
+		Topic:     cfg.Client.Topic(cfg.TopicID),
+		Timeout:   cfg.Timeout,
+		TopicID:   cfg.TopicID,
+		ProjectID: cfg.ProjectID,
+	}
+}
+
+func (p *Pub) Publish(ctx context.Context, i interface{}) error {
+	b, err := json.Marshal(i)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	logx.WithContext(ctx).WithFields(logrus.Fields{
+		"topicID":   p.TopicID,
+		"projectID": p.ProjectID,
+		"value":     logx.LimitMSG(b),
+	}).Info("publish information")
+
+	pubCtx, cancel := context.WithTimeout(context.Background(), p.Timeout)
+	defer cancel()
+
+	res := p.Topic.Publish(pubCtx, &pubsub.Message{Data: b})
+	if _, err = res.Get(pubCtx); err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
--- pubsubx/pubsubx.go
+++ pubsubx/pubsubx.go
@@ -1,69 +1,37 @@
-package pub
+package pubsubx
 
 import (
 	"context"
-	"encoding/json"
 	"time"
 
 	"cloud.google.com/go/pubsub"
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"github.com/tOnkowzl/libs/logx"
 	"google.golang.org/api/option"
 )
 
-type Config struct {
+type ClientConfig struct {
+	Pool        int
 	Timeout     time.Duration
 	ProjectID   string
-	TopicID     string
 	Credentials string
 }
 
-type Pub struct {
-	Topic     *pubsub.Topic
-	Timeout   time.Duration
-	TopicID   string
-	ProjectID string
-}
+func NewClient(cfg *ClientConfig) *pubsub.Client {
+	if cfg.Pool == 0 || cfg.Timeout == 0 || cfg.ProjectID == "" || cfg.Credentials == "" {
+		logx.Panic("pubsub pool, timeout, projectID, credentails required")
+	}
 
-func NewPub(cfg Config) *Pub {
 	ctx, cancel := context.WithTimeout(context.Background(), cfg.Timeout)
 	defer cancel()
 
 	client, err := pubsub.NewClient(ctx,
 		cfg.ProjectID,
-		option.WithCredentialsJSON([]byte(cfg.Credentials)))
+		option.WithCredentialsJSON([]byte(cfg.Credentials)),
+		option.WithGRPCConnectionPool(cfg.Pool),
+	)
 	if err != nil {
 		logx.Panic(err)
 	}
 
-	return &Pub{
-		Topic:     client.Topic(cfg.TopicID),
-		Timeout:   cfg.Timeout,
-		TopicID:   cfg.TopicID,
-		ProjectID: cfg.ProjectID,
-	}
-}
-
-func (p *Pub) Publish(ctx context.Context, i interface{}) error {
-	b, err := json.Marshal(i)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	logx.WithContext(ctx).WithFields(logrus.Fields{
-		"topicID":   p.TopicID,
-		"projectID": p.ProjectID,
-		"value":     logx.LimitMSG(b),
-	}).Info("publish information")
-
-	pubCtx, cancel := context.WithTimeout(context.Background(), p.Timeout)
-	defer cancel()
-
-	res := p.Topic.Publish(pubCtx, &pubsub.Message{Data: b})
-	if _, err = res.Get(pubCtx); err != nil {
-		return errors.WithStack(err)
-	}
-
-	return nil
+	return client
 }

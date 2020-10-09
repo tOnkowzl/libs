495b53505d18c8530e03f6f5e27b53eaccf24d12
tOnkowzl <wpansophon@gmail.com>
2020-10-09 12:10:32 +0700

    refactor pubsubx

--- pubsubx/pub.go
+++ pubsubx/pub.go
@@ -19,33 +19,31 @@ type PubConfig struct {
 }
 
 type Pub struct {
-	Topic     *pubsub.Topic
+	Client    *pubsub.Client
 	Timeout   time.Duration
-	TopicID   string
 	ProjectID string
 }
 
 func NewPub(cfg PubConfig) *Pub {
-	if cfg.Client == nil || cfg.Timeout == 0 || cfg.TopicID == "" || cfg.ProjectID == "" {
+	if cfg.Client == nil || cfg.Timeout == 0 || cfg.ProjectID == "" {
 		logx.Panic("pubsub client, timeout, topicID, projectID required")
 	}
 
 	return &Pub{
-		Topic:     cfg.Client.Topic(cfg.TopicID),
+		Client:    cfg.Client,
 		Timeout:   cfg.Timeout,
-		TopicID:   cfg.TopicID,
 		ProjectID: cfg.ProjectID,
 	}
 }
 
-func (p *Pub) Publish(ctx context.Context, i interface{}) error {
+func (p *Pub) Publish(ctx context.Context, topicID string, i interface{}) error {
 	b, err := json.Marshal(i)
 	if err != nil {
 		return errors.WithStack(err)
 	}
 
 	logx.WithContext(ctx).WithFields(logrus.Fields{
-		"topicID":   p.TopicID,
+		"topicID":   topicID,
 		"projectID": p.ProjectID,
 		"value":     logx.LimitMSG(b),
 	}).Info("pub information")
@@ -53,8 +51,9 @@ func (p *Pub) Publish(ctx context.Context, i interface{}) error {
 	pubCtx, cancel := context.WithTimeout(context.Background(), p.Timeout)
 	defer cancel()
 
-	res := p.Topic.Publish(pubCtx, &pubsub.Message{Data: b})
-	if _, err = res.Get(pubCtx); err != nil {
+	topic := p.Client.Topic(topicID)
+
+	if _, err = topic.Publish(pubCtx, &pubsub.Message{Data: b}).Get(pubCtx); err != nil {
 		return errors.WithStack(err)
 	}
 
